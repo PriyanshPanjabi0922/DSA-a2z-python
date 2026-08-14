889a597a5862fdc2b3a106b25adf84a0b2339949
PriyanshPanjabi0922 <priyanshpanjabi@gmail.com>
2026-08-14 20:02:31 +0530

    Add one array easy problem

---
+++ Array/Easy/Problem_05.py
@@ -0,0 +1,15 @@
+## Problem: Left Rotate an array by one place
+
+arr = [1,2,3,4,5]
+n = len(arr)
+def left_rotate(arr,n):
+    temp = arr[0]
+
+    for i in range(n):
+        arr[i-1] = arr[i]
+
+    arr[i-1] = temp
+
+    print(arr)
+
+left_rotate(arr,n)
